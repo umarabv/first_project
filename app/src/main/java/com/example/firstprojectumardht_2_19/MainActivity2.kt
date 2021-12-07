27bfe51e707b7c8af4d22cf0c41eb062bd66889d
umarabv <umar.abv0311@gmail.com>
2021-12-08 02:23:00 +0600

    committt

---
+++ app/src/main/java/com/example/firstprojectumardht_2_19/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.firstprojectumardht_2_19
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val btn = findViewById<Button>(R.id.buttom)
+
+        btn.setOnClickListener {
+            startActivity(Intent(this, MainActivity3::class.java))
+        }
+    }
+}
